4fdf4be49cf387cf3135cb1b973bbc30f329c226
Zouhairzed <59532744+Zouhairzed@users.noreply.github.com>
2021-10-18 18:55:05 +0200

    Update main.tf

--- Exercices/8 - datasources/main.tf
+++ Exercices/8 - datasources/main.tf
@@ -1,3 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.78.0"
+    }
+  }
+}
+
+
 data "azurerm_storage_container" "datacontainer" {
   name                 = "tfstate"
   storage_account_name = "tfstatestorageaccountz"
